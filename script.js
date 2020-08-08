a33a8fa23bc89a4820ced02f61c1c17d79e16421
qodirovshohijahon <tasaddux@gmail.com>
2020-08-08 13:30:45 +0500

    feat: Several functions created for Tetris game

--- script.js
+++ script.js
@@ -44,18 +44,102 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const theTetri = [lTetrimino, zTetrimino, tTetrimino, oTetrimino, iTetrimino];
 
-    let currentPosition = 4;
-    let current = theTetri[0];
+    //Elementni burish
 
+    let currentPosition = 4;
+    let currentRotation = 0;
+    let random = Math.floor(Math.random() * theTetri.length)
+    let current = theTetri[random][0];
     console.log(current);
 
     //draw the fitst rotation
 
     function draw() {
         current.forEach(index => {
-            squares[currentPosition + index].classList.add('tetrimino')
+          squares[currentPosition + index].classList.add('tetrimino')
+         // console.log("dra", currentPosition + index)
+ 
         })
     }
 
-    draw();
+  //  draw();
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetrimino');
+        //console.log("und", currentPosition + index)
+        })
+    }
+
+    //element ni pastga tushutish
+    let timerId = setInterval(moveDown, 1000)
+
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if(e.keyCode === 39) {
+            moveRight()
+        } else if(e.keyCode === 39) {
+            //
+        } else if(e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keyup', control)
+
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+            //yangi tetris boshlash
+            random = Math.floor(Math.random() * theTetri.length)
+            current = theTetri[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    //chegarag yetganda o'ngga burilishdan to'xtatish
+
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index ) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -= 1;
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+        draw()
+    }
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+        
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -= 1;
+        }
+        draw()
+    }
+
+
+    //tetrisni burish
+
+    function rotate() {
+        undraw()
+        currentPosition ++
+        if(currentRotation === current.length) { //agar currentRotation  = 4 bo'lsa 4 ga aylantir
+            currentRotation = 0
+        }
+    }
 })
