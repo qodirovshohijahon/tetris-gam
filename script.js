93ea818c0670f9007edd4780c535abf66f92b76d
qodirovshohijahon <tasaddux@gmail.com>
2020-08-09 10:53:57 +0500

    refactor: Game is almost ready to playy))

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const grid = document.querySelector('.grid');
     let squares = Array.from(document.querySelectorAll('.grid div'));
-    const ScoreDisplay = document.querySelector('#score');
-    const StartButton = document.querySelector('#start-button');
+    const scoreDisplay = document.querySelector('#score');
+    const startButton = document.querySelector('#start-button');
     const width = 10;
-
+    let nextRandom = 0;
+    let timerId;
+    let score = 0;
     //tetrimino
 
     const lTetrimino = [
@@ -72,15 +74,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     //element ni pastga tushutish
-    let timerId = setInterval(moveDown, 1000)
+//    let timerId = setInterval(moveDown, 1000)
 
     function control(e) {
         if(e.keyCode === 37) {
             moveLeft()
+        } else if(e.keyCode === 38) {
+            rotate()
         } else if(e.keyCode === 39) {
             moveRight()
-        } else if(e.keyCode === 39) {
-            //
         } else if(e.keyCode === 40) {
             moveDown()
         }
@@ -100,10 +102,13 @@ document.addEventListener('DOMContentLoaded', function() {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
 
             //yangi tetris boshlash
-            random = Math.floor(Math.random() * theTetri.length)
+            random = nextRandom
+            nextRandom = Math.floor(Math.random() * theTetri.length)
             current = theTetri[random][currentRotation]
             currentPosition = 4
             draw()
+            displayShape()
+            addScore();
         }
     }
 
@@ -137,9 +142,65 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function rotate() {
         undraw()
-        currentPosition ++
+        currentRotation ++
         if(currentRotation === current.length) { //agar currentRotation  = 4 bo'lsa 4 ga aylantir
             currentRotation = 0
+        }
+        current = theTetri[random][currentRotation]
+        draw()
+    }
+
+
+    //keyingi shaklni ko'rsatish
+    const displaySquares = document.querySelectorAll('mini-grid div')
+    const displayWidth = 4;
+    let displayIndex = 0;
+
+    const upNextTetri = [
+        [1, displayWidth + 1, displayWidth * 2 + 1, 2],
+        [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1],
+        [1, displayWidth, displayWidth + 1, displayWidth + 2],
+        [0, 1, displayWidth, displayWidth + 1],
+        [1, displayWidth + 1, displayWidth * 2 + 1,displayWidth * 3 + 1],
+    ]
+
+    //    kichik shaklni ifodalash
+    function displayShape() {
+        displaySquares.forEach(square => {
+            square.classList.remove('tetrimino')
+        })
+        upNextTetri[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add('tetrimino')
+        })
+    }
+
+    startButton.addEventListener('click', () => {
+        if(timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random() * theTetri.length)
+            displayShape()
+        }
+    })
+
+    function addScore() {
+        for(let i = 0; i < 199; i += width) {
+            const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i+ 7, i + 8, i + 9];
+
+            if(row.every(index => squares[index].classList.contains('taken'))) {
+                score += 10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                })
+                const squaresRemoved = squares.splice(i, width)
+//                console.log(squaresRemoved)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
         }
     }
 })
